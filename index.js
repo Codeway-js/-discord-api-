40491a495f51fae1ddfabf3835c76138092923b1
leblanmonster <73737394+leblanmonster@users.noreply.github.com>
2021-10-27 18:26:22 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+ axios.get("https://discord.com/api/v8/channels/tonchan id/messages",{
+     headers:{ Authorization: 'Bot '+tontoken, 'Content-Type': 'application/json'},
+     data:{content:"hello world}),
+     credentials: 'include'
+ })
